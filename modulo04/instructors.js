64d1d358dfba3f07f131e992bb5be18fc1f24d6d
bruno-victor32 <bruno.victor32@yahoo.com.br>
2020-12-06 22:11:56 -0300

    CONTINUAÇÃO DOS ESTUDOS

--- modulo04/instructors.js
+++ modulo04/instructors.js
@@ -3,6 +3,28 @@ module.exports ={}*/
 
 const fs = require('fs')/*Um modulo do node,que vai trabalhar com arquivos do sistema*/
 const data = require("./data.json")
+
+/*Show*/
+exports.show = function (req, res) {
+        /*req.params*/
+        const{id} = req.params
+
+        const foundInstructor = data.instructors.find(function(instructor){
+        return instructor.id == id
+        })
+        if (!foundInstructor) return res.send("Instructor not found")
+
+        const instructor = {
+            ...foundInstructor,
+            age: "",
+            gender:"",
+            services:foundInstructor.services,
+            created_at:"",
+        }
+
+        return res.render("instructors/show",{instructor})
+}
+
 /*Create*/
 exports.post = function (req, res) {
     /*req.query*/
@@ -15,6 +37,7 @@ exports.post = function (req, res) {
     //req.body ele retornou isso
     //{"avatar_url":"https://www.techtudo.com.br/noticias/2020/08/dell-g3-vs-lenovo-ideapad-gaming-3i-saiba-qual-notebook-gamer-e-melhor.ghtml","name":"Bruno Victor da Silva Vicente","birth":"2020-08-06","gender":"on","services":"kkkkkkkkkkk"}
 
+/*VALIDAÇÃO DOS DADOS*/
 
     //["avatar_url","name","birth","gender","services"]
     /*Aqui eu fiz que todos os cantos são obrigatorios*/
@@ -27,12 +50,29 @@ exports.post = function (req, res) {
         }
     }
 
-    req.body.birth = Date.parse(req.body.birth)
-    req.body.created_at = Date.now() /*Date.now e um metodo que vai criar uma data de agora,do momento que está sendo salvo.Ele vai criar uma data no formato númerica,um número que vai valer de milisegundos */
-    req.body.id = Number(data.instructors.length + 1)
+    let {avatar_url, birth, name, services, gender} = req.body /*Vai pegar de dentro do req.body,vai desestruturar e vai pegar somente o que eu estou pedindo que nesse caso e o avatar_url*/
 
+/*TRATAMENTOS DOS DADOS*/
+
+    birth = Date.parse(birth)
+    const created_at = Date.now() /*Date.now e um metodo que vai criar uma data de agora,do momento que está sendo salvo.Ele vai criar uma data no formato númerica,um número que vai valer de milisegundos */
+    const id = Number(data.instructors.length + 1)
+
+/*ORGANIZANDO OS DADOS QUE QUERO ENVIAR PARA DENTRO*/
+
+   
+
+    data.instructors.push({
+        id,
+        avatar_url,
+        name,
+        birth,
+        gender,
+        services,
+        created_at,
+    })
     /*Tenho um array[] e ele está vazio*/
-    data.instructors.push(req.body)/*Quando ele rodar o push ele vai voltar como um array nesse caso como um objeto[{...}] ai eu vou salvar novamente.Da proxima vez o data.instructors já vai ter um ali.A segunda vez que eu adicionar um req.body ele vai adicionar mais uma entrada*/ 
+   /* data.instructors.push(req.body)*//*Quando ele rodar o push ele vai voltar como um array nesse caso como um objeto[{...}] ai eu vou salvar novamente.Da proxima vez o data.instructors já vai ter um ali.A segunda vez que eu adicionar um req.body ele vai adicionar mais uma entrada*/ 
 
     fs.writeFile("data.json", JSON.stringify(data, null, 2),function(err){//Uma callback function ela vai ter 2 caracteristicas,uma delas e executar depois de um certo tempo e a segunda e uma função que passo dentro de uma função
         if(err) return res.send("Erro de escrita")
